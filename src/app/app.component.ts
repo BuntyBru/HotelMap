5135f42c258b985ef33306a9b07e26149e0358fe
Buntybru <buntybru@gmail.com>
2019-12-06 18:19:52 +0530

    Added the list and stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BackService} from './back.service';
 import {ListComponent} from './list/list.component';
+declare var google;
 
 @Component({
   selector: 'app-root',
